dbde5f22a1f09ffb5ff0c77f867c3bf2419c3124
Joan Areste Gonzalez <jareste2000@gmail.com>
2023-09-12 01:23:38 +0200

    initial tests ex04

---
+++ ex04/Sed.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Sed.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jareste- <jareste-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 01:07:21 by jareste-          #+#    #+#             */
+/*   Updated: 2023/09/12 01:10:58 by jareste-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SED_H
+# define SED_H
+
+# include <iostream>
+
+# define INCORRECT_INPUT "Incorrect input, try \"./Sed_is_for_losers filename \"string1\" \"string2\" \""
+
+#endif
